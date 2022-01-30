1847b8eb71e6e013738e1f51197dffeb761f2eac
ElianeCristina <96190678+ElianeCristina@users.noreply.github.com>
2022-01-29 21:20:19 -0300

    Add files via upload

--- pj_apod.js
+++ pj_apod.js
@@ -1,3 +1,15 @@
-function(){
-    
+$("submit").click(function(event){
+    event.preventDefault();
+    pesquisar();
+})
+
+function pesquisar(){
+    var data = document.getElementById("data").value;
+    $.ajax({url: `https://api.nasa.gov/planetary/apod?api_key=dAIKxaQYznpQC4x3cJilLzKlifcdHTTzo5Xp5y1e&date=${data}`,
+    success: function(data) {
+        $('#img').attr('src', data.url) 
+        $('#title').text(data.title)
+        $('#explanation').text(data.explanation)
+        $('#copyright').text(data.copyright);
+    }})
 }
